1315ebfcdbb5ebe0ddf93216329da52a51ac1d2a
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-07-23 23:55:11 -0400

    Add profile component

---
+++ src/Profile.jsx
@@ -0,0 +1,28 @@
+
+
+const Profile = () => {
+
+    return(
+        <div>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name"  />
+
+             <label htmlFor="phone">Phone Number</label>
+            <input type="text" id="phone"  />
+
+             <label htmlFor="email">Email</label>
+            <input type="text" id="email"  />
+
+             <label htmlFor="portfolio">Portfolio</label>
+            <input type="text" id="portfolio"  />
+
+             <label htmlFor="github">Github</label>
+            <input type="text" id="github"  />
+
+             <label htmlFor="linkedin">Name</label>
+            <input type="text" id="linkedin"  />
+        </div>
+    )
+}
+
+export default Profile;
