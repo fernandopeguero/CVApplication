1171bf671f0a708703da99c7e01dfacb4c20d212
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-07-24 22:36:46 -0400

    Add education component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Header  from './Header'
 import Profile from './Profile'
+import Education from './Education'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>  
       <Header />
       <Profile />
+      <Education />
     </>
   )
 }
---
+++ src/Education.jsx
@@ -0,0 +1,28 @@
+
+
+const Education = () => {
+
+    return(
+        <div>
+            <div>
+                <h2>Education</h2>
+                <i>^</i>
+            </div>
+            <label htmlFor="school">School</label>
+            <input type="text" id="school" />
+
+            <label htmlFor="degree">Degree/Certificates</label>
+            <input type="text" id="degree" />
+
+            <label htmlFor="from_date">From</label>
+            <input type="date" id="from_date" />
+
+            <label htmlFor="to_date">To</label>
+            <input type="date" id="to_date" />
+
+        </div>
+    )
+}
+
+
+export default Education;
