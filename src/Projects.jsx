e0a08c500cff6e2840e875d61e18cd149d2f741a
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-07-30 00:14:40 -0400

    ADD toggle functionality to project component

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,19 +1,30 @@
-
+import projectIcon from "./assets/project.svg";
+import { useState } from "react";
+import arrowUp from "./assets/up-arrow.svg";
+import downArrow from "./assets/down-arrow.svg";
 
 const Projects = () => {
 
+    const [expanded, setExpanded] = useState(false);
+
     return(
         <div className="card">
-            <div className="title">
-                <h2>Projects</h2>
-                
+            <div className="title" onClick={() => setExpanded(!expanded)}>
+                <img className="section_logo" src={projectIcon} alt="" />
+                <h2>Profile</h2>
+                <img className="toggler" src={expanded ? downArrow : arrowUp} alt=""/>             
             </div>
 
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" />
+            {expanded && (
+                <>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" />
 
-            <label htmlFor="link">Link</label>
-            <input type="url" />
+                    <label htmlFor="link">Link</label>
+                    <input type="url" />
+                </>
+            )}
+
 
             
         </div>
