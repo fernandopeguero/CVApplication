940cce91d7736cc1cbe3d25b59fff40d4258d1b0
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-07-30 00:24:09 -0400

    ADD toggle functionality to education component

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,24 +1,37 @@
-
+import { useState } from "react";
+import arrowUp from "./assets/up-arrow.svg";
+import downArrow from "./assets/down-arrow.svg";
+import workHistory from "./assets/work_history.svg";
 
 const Education = () => {
 
+    const [expanded, setExpanded] = useState(false);
+
     return(
         <div className="card">
-            <div className="title">
+            <div className="title" onClick={() => setExpanded(!expanded)}>
+                <img className="section_logo" src={workHistory} alt="" />
                 <h2>Education</h2>
-                <i>^</i>
+                <img className="toggler" src={expanded ? downArrow : arrowUp} alt=""/>             
             </div>
-            <label htmlFor="school">School</label>
-            <input type="text" id="school" />
 
-            <label htmlFor="degree">Degree/Certificates</label>
-            <input type="text" id="degree" />
+            {expanded && (
 
-            <label htmlFor="from_date">From</label>
-            <input type="date" id="from_date" />
+                <>
+                    <label htmlFor="school">School</label>
+                    <input type="text" id="school" />
 
-            <label htmlFor="to_date">To</label>
-            <input type="date" id="to_date" />
+                    <label htmlFor="degree">Degree/Certificates</label>
+                    <input type="text" id="degree" />
+
+                    <label htmlFor="from_date">From</label>
+                    <input type="date" id="from_date" />
+
+                    <label htmlFor="to_date">To</label>
+                    <input type="date" id="to_date" />
+                </>
+            )}
+
 
         </div>
     )
