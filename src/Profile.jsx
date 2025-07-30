5fd17d5c73ea085fab55e4523d5bc69f87e171fc
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-07-30 00:04:53 -0400

    ADD expanded functinality to card

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,35 +1,44 @@
 import profile_icon from "./assets/account_profile.svg"
+import arrowUp from "./assets/up-arrow.svg";
+import downArrow from "./assets/down-arrow.svg";
+import { useState } from "react";
 
 const Profile = ({state, setState}) => {
 
+    const [expanded, setExpanded] = useState(false);
+
 
     return(
-        <div className="card">
-            <div className="title">
+        <div className="card" >
+            <div className="title" onClick={() => setExpanded(!expanded)}>
                 <img className="section_logo" src={profile_icon} alt="" />
                 <h2>Profile</h2>
-                 
+                <img  src={expanded ? downArrow : arrowUp} alt=""/>             
             </div>
-           
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" autoComplete="name" value={state.name}  onChange={ (e) => { setState({...state, name: e.target.value}) }}/>
+            {expanded && (
+                <>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" autoComplete="name" value={state.name}  onChange={ (e) => { setState({...state, name: e.target.value}) }}/>
 
-             <label htmlFor="phone">Phone Number</label>
-            <input type="text" id="phone" autoComplete="phone" value={state.phone} onChange={ (e) => { setState({...state, phone: e.target.value}) }} />
+                    <label htmlFor="phone">Phone Number</label>
+                    <input type="text" id="phone" autoComplete="phone" value={state.phone} onChange={ (e) => { setState({...state, phone: e.target.value}) }} />
 
-             <label htmlFor="email">Email</label>
-            <input type="text" id="email" autoComplete="email" value={state.email} onChange={ (e) => { setState({...state, email: e.target.value})}}  />
+                    <label htmlFor="email">Email</label>
+                    <input type="text" id="email" autoComplete="email" value={state.email} onChange={ (e) => { setState({...state, email: e.target.value})}}  />
 
-             <label htmlFor="portfolio">Portfolio</label>
-            <input type="text" id="portfolio" autoComplete="portfolio" value={state.portfolio} onChange={(e) => {setState({...state, portfolio: e.target.value})}} />
+                    <label htmlFor="portfolio">Portfolio</label>
+                    <input type="text" id="portfolio" autoComplete="portfolio" value={state.portfolio} onChange={(e) => {setState({...state, portfolio: e.target.value})}} />
 
-             <label htmlFor="github">Github</label>
-            <input type="text" id="github"  autoComplete="github"  value={state.github} onChange={(e) => {setState({...state, github: e.target.value})}}/>
+                    <label htmlFor="github">Github</label>
+                    <input type="text" id="github"  autoComplete="github"  value={state.github} onChange={(e) => {setState({...state, github: e.target.value})}}/>
 
-             <label htmlFor="linkedin">Linkedin</label>
-            <input type="text" id="linkedin" autoComplete="linkedin" value={state.linkedin} onChange={(e) => {setState({...state, linkedin: e.target.value})}} />
+                    <label htmlFor="linkedin">Linkedin</label>
+                    <input type="text" id="linkedin" autoComplete="linkedin" value={state.linkedin} onChange={(e) => {setState({...state, linkedin: e.target.value})}} />
 
-            <button>Add Profile</button>
+                    <button>Add Profile</button>
+            </>
+            )}
+
         </div>
     )
 }
